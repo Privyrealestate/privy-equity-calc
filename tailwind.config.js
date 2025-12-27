cc677f8117d64c7e157c895426207c353a2d7408
Privyrealestate <privyrealestate@gmail.com>
2025-12-27 10:33:07 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,5 @@
 module.exports = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        privy: {
-          dominion: '#0A291E',
-          ledger: '#F2F0EC',
-          copper: '#B87D4B',
-          vigilance: '#E04F39',
-        }
-      },
-      fontFamily: {
-        sans: ['var(--font-libre-franklin)'],
-        serif: ['var(--font-crimson-pro)'],
-        mono: ['var(--font-space-mono)'],
-      },
-    },
-  },
-  plugins: [],
-};
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
